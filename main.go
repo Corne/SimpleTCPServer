9f2e285a6b9e5ea259b03f00e3fc685ee98b573f
Corne van Beek <cornevbeek123@live.nl>
2015-07-30 22:02:54 +0200

    implemented basic tcpserver, so it sends basic messages to all clients

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 )
 
 const (
 	CONN_HOST = "localhost"
-	CONN_PORT = "3333"
 	CONN_TYPE = "tcp"
 )
 
+var (
+	channels []chan string = []chan string{}
+)
+
 func main() {
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+
+	//read port
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Port: ")
+	port, err := reader.ReadString('\n')
+	port = strings.TrimSpace(port)
+
 	if err != nil {
-		fmt.Println("Error: ", err.Error())
-		return
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
+	}
+	// listen on given  localhost to given port
+	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+port)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		os.Exit(1)
 	}
 
-	defer l.Close()
-	fmt.Printf("Listening on %s:%s\n", CONN_HOST, CONN_PORT)
+	defer listener.Close()
+	fmt.Printf("Listening on %s:%s\n", CONN_HOST, port)
 
+	go readInput()
+	// handle each incoming request
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("Error accepting: %s\n", err.Error())
 			continue
@@ -32,7 +52,46 @@ func main() {
 	}
 }
 
+func readInput() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			return
+		}
+		for _, c := range channels {
+			c <- message
+		}
+	}
+}
+
 func handleRequest(conn net.Conn) {
-	conn.Write([]byte("Hello."))
+	c := make(chan string)
+	channels = append(channels, c)
+
+	for {
+		message := <-c
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			break
+		}
+	}
+
+	i := indexOf(c)
+	fmt.Println(i)
+	channels = append(channels[:i], channels[i+1:]...)
+
 	conn.Close()
+}
+
+func indexOf(val chan string) int {
+	for i, el := range channels {
+		if el == val {
+			return i
+		}
+	}
+	return -1
 }
